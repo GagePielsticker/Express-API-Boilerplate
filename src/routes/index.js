5a62396c9db97fd00d7daebecc26da20d7e3cffd
Gage Pielsticker <gagepays@gmail.com>
2020-08-26 03:48:50 -0500

    Added an xss filter as well as express-validator for payload validation
    examples documented in code of index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,27 @@
  */
 const express = require('express')
 const router = express.Router()
+const xss = require('xss')
+const { body, validationResult } = require('express-validator')
 
 module.exports = client => { // The reason we pass client is to access engine functionality and methods
-  router.get('/', async (req, res) => {
+  router.get('/', [
+
+    // body('username').isEmail(), example payload validation
+    // body('password').isLength({ min: 5 })
+
+  ], async (req, res) => {
+    // xss(req.param) example xss filter
+
+    /*
+    If payload validation fails or errors, the server will respond
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    */
+
     res.send('Welcome to the api.')
   })
 

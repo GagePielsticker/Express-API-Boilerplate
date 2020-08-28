75ba414c4ccff5249ea6428c773e1b51782be135
Gage Pielsticker <gagepays@gmail.com>
2020-08-28 05:05:39 -0500

    Bugfix

--- test/example.test.js
+++ test/example.test.js
@@ -18,7 +18,7 @@ beforeAll(async () => {
   /* Connect database */
   await client.connectDatabase()
     .then(() => console.log('Database connected.'))
-    .catch(e => console.log(`Could not connect to database. : ${e}`))
+    .catch(e => { throw new Error('Could not connect to database') })
 }, 10000) // 10 second timeout connecting to db
 
 afterAll(async () => {
